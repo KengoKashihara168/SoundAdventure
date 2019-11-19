88ed1bac54755cb821e2ab78ce1971978ff44634
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-11-19 18:24:18 +0900

    汎用クラスの作成中

---
+++ Assets/Kashihara/Scripts/Chip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChipType
+{
+    None,
+    jizo,
+    Exit,
+    Hassyaku,
+}
+
+public class Chip : MonoBehaviour
+{
+    public ChipType type { get; set; }
+    private Sound sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Sound GetSound()
+    {
+        return sound;
+    }
+}
