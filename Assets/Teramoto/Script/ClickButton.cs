b1ec9596cc34cb456659979d939301ef2dd7180b
teramoti <teramoti0315@icloud.com>
2019-12-17 13:59:33 +0900

    あげなおし

--- Assets/Teramoto/Script/ClickButton.cs
+++ Assets/Teramoto/Script/ClickButton.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickSettingPlayerButton : MonoBehaviour
+public class ClickButton : MonoBehaviour
 {
     //ボタンが押されたかのFlag
     bool IsClickButton;
