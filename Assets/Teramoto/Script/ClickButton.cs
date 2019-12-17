2ca7cb253fcd5e9f370ab4d43e99ad6181eb5887
teramoti <teramoti0315@icloud.com>
2019-12-17 11:46:21 +0900

    青年と憑人かどうかのScriptと場所決め

---
+++ Assets/Teramoto/Script/ClickButton.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSettingPlayerButton : MonoBehaviour
+{
+    //ボタンが押されたかのFlag
+    bool IsClickButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsClickButton = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsClickButton = false;
+
+
+    }
+
+    public void OnClickButton()
+    {
+        IsClickButton = true;
+    }
+
+    public bool GetClickButton()
+    {
+        return IsClickButton;
+    }
+}
---
+++ Assets/Teramoto/Script/ClickSettingPlayerButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    //ボタンが押されたかのFlag
+    bool IsClickButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsClickButton = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsClickButton = false;
+
+
+    }
+
+    public void OnClickButton()
+    {
+        IsClickButton = true;
+       // Debug.Log("aaaa");
+    }
+
+    public bool GetClickButton()
+    {
+        return IsClickButton;
+    }
+}
