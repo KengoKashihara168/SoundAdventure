5d882e56d0af5e46602d6a1a4a4e0b4e31582d3b
TSITCL\s172080 <kbysaaao@gmail.com>
2019-12-23 09:17:04 +0900

    コミット

--- Assets/Kobayashi/Script/Jugment.cs
+++ Assets/Kobayashi/Script/Jugment.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class Jugment : MonoBehaviour
 {
+    [SerializeField]
+    private Text[] player = new Text[4];
+    //プレイヤーの今の状態を知るための変数
+    string[] nowState = new string[4];
+    // プレイヤーの参加人数
+    [SerializeField]
+    private int playerNum = 4;
     //アイテムの獲得---
     //画像の表示
     private Image itemImage;
@@ -21,14 +28,21 @@ public class Jugment : MonoBehaviour
     private Button yesButton;
     [SerializeField]
     private Button noButton;
+
+    [SerializeField]
+    private Button nextButton;
     //-----------------
     //全体の結果表示---
     [SerializeField]
     private Text playerText;
+
+    bool itemGetFlag;
+    bool goalArriveFlag;
+    bool goalUnlockFlag;
     //プレイヤーナンバー
-    private string playerNum;
-    //プレイヤーの今の状態を知るための変数
-    string nowState;
+    private string playerNumText;
+    //プレイヤーの今の情報を知るための変数
+    string nowInformation;
     [SerializeField]
     //プレイヤーの今の状態を表示
     private Text state;
@@ -37,30 +51,85 @@ public class Jugment : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        for (int i = 0; i < playerNum; i++)
+        {
+            player[i].gameObject.SetActive(false);
+            nowState[i] = "生存";
+        }
         //画像とテキストを表示
         itemImage = GetComponent<Image>();
-        itemImage.gameObject.SetActive(true);
-        itemText.gameObject.SetActive(true);
-        text.gameObject.SetActive(true);
-        yesButton.gameObject.SetActive(true);
-        noButton.gameObject.SetActive(true);
+        itemImage.enabled = false;
+        itemText.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+        yesButton.gameObject.SetActive(false);
+        noButton.gameObject.SetActive(false);
+        nextButton.gameObject.SetActive(false);
         //画像とテキストを非表示
         playerText.gameObject.SetActive(false);
         state.gameObject.SetActive(false);
         //最初は生存
-        nowState = "生存";
-        playerNum = "1";
+        itemText.GetComponent<Text>().text = "ネコ";
+        nowInformation = "何も無かった";
+        playerNumText = "1";
+        itemGetFlag = false;
+        goalArriveFlag = false;
+        goalUnlockFlag = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //アイテムの画像情報を取得
-        ItemImageGet();
-        //プレイヤーの情報(ナンバーを取得)
-        PlayerNumGet();
-        //画面にプレイヤーと今の状態を表示する
-        PlayerNewState();
+        //アイテムがあるマスに止まったら
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //アイテムの画像情報を取得
+            ItemGet();
+            ItemImageGet();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Judgment();
+            //プレイヤーの情報(ナンバーを取得)
+            PlayerNumGet();
+            PlayerNewInformation();
+        }
+    }
+    
+    void ItemGet()
+    {
+        itemImage.enabled = true;
+        itemText.gameObject.SetActive(true);
+        text.gameObject.SetActive(true);
+        yesButton.gameObject.SetActive(true);
+        noButton.gameObject.SetActive(true);
+    }
+
+
+    //アイテムの画像情報を取得
+    void ItemImageGet()
+    {
+        //キーを押したら画像が切り替わる
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            itemImage.sprite = itemSprite[0];
+            itemText.GetComponent<Text>().text = "ネコ";
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            itemImage.sprite = itemSprite[1];
+            itemText.GetComponent<Text>().text = "ワシ";
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            itemImage.sprite = itemSprite[2];
+            itemText.GetComponent<Text>().text = "キツネ";
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            itemImage.sprite = itemSprite[3];
+            itemText.GetComponent<Text>().text = "ラッコ";
+        }
     }
 
     //全体の結果を表示
@@ -70,31 +139,11 @@ public class Jugment : MonoBehaviour
         playerText.gameObject.SetActive(true);
         state.gameObject.SetActive(true);
         //プレイヤーナンバーを表示
-        playerText.GetComponent<Text>().text = "Player" + playerNum.ToString();
-        //今のプレイヤーの状態を表示
-        state.GetComponent<Text>().text = nowState.ToString();
-    }
-
-    //アイテムの画像情報を取得
-    void ItemImageGet()
-    {
-        //キーを押したら画像が切り替わる
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            itemImage.sprite = itemSprite[0];
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            itemImage.sprite = itemSprite[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            itemImage.sprite = itemSprite[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            itemImage.sprite = itemSprite[1];
-        }
+        playerText.GetComponent<Text>().text = "Player" + playerNumText.ToString();
+       
+        //画面にプレイヤーと今の状態を表示する
+        PlayerNewInformation();
+        nextButton.gameObject.SetActive(true);
     }
 
     //プレイヤーの情報(ナンバーを取得)
@@ -103,50 +152,81 @@ public class Jugment : MonoBehaviour
         //キーを押したら画像が切り替わる
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            playerNum = "1";
+            playerNumText = "1";
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            playerNum = "2";
+            playerNumText = "2";
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            playerNum = "3";
+            playerNumText = "3";
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            playerNum = "4";
+            playerNumText = "4";
         }
     }
 
-    //画面にプレイヤーと今の状態を表示する
-    void PlayerNewState()
+    //プレイヤーが知った情報を表示
+    void PlayerNewInformation()
     {
-        //プレイヤーがゴールしたら
+        //アイテムを獲得
+        if (itemGetFlag == true)
+        {
+            nowInformation = "アイテム獲得";
+        }
+
+        //ゴールがあるマスに止まって鍵が開いていたら
+        if (goalArriveFlag == true && goalUnlockFlag == true)
+        {
+            nowInformation = "ゴールにたどり着きました";
+        } //ゴールがあるマスに止まって鍵が閉まっていたら
+        else if (goalArriveFlag == true && goalUnlockFlag == false)
+        {
+            nowInformation = "ゴールにたどり着きました\n鍵が掛かっています";
+        }
+
+        //アイテムを獲得
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            nowState = "ゴール";
+            nowInformation = "アイテム獲得";
         }
-        else
+
+        //ゴールがあるマスに止まって鍵が開いていたら
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            ////まだ生存していたら
-            //if (Input.GetKeyDown(KeyCode.DownArrow))
-            //{
-            //    nowState = "生存";
-            //}
-            //else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            //{
-            //    nowState = "死亡";
-            //}
+            nowInformation = "ゴールにたどり着きました";
+        } //ゴールがあるマスに止まって鍵が閉まっていたら
+        else// if (goalArriveFlag == true && goalUnlockFlag == false)
+        {
+            nowInformation = "ゴールにたどり着きました\n鍵が掛かっています";
         }
-        //まだ生存していたら
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        //今のプレイヤーの状態を表示
+        state.GetComponent<Text>().text = nowInformation.ToString();
+    }
+    //プレイヤーの今の状態を表示
+    void PlayerNewState()
+    {
+        //画像とテキストを非表示
+        playerText.gameObject.SetActive(false);
+        state.gameObject.SetActive(false);
+        for (int i = 0; i < playerNum; i++)
         {
-            nowState = "生存";
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            nowState = "死亡";
+            nowState[i] = "生存";
+            //まだ生存していたら
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                nowState[i] = "ゴール";
+            }
+            else
+            {
+                nowState[i] = "死亡";
+            }
+
+            //画面にプレイヤーと今の状態を表示する
+            player[i].GetComponent<Text>().text = "Player" + (i + 1).ToString() + " " + nowState[i].ToString();
         }
     }
 
@@ -154,8 +234,9 @@ public class Jugment : MonoBehaviour
     public void YesButton()
     {
         Debug.Log("クリックされた");
+        itemGetFlag = true;
         //画像とテキストを非表示
-        itemImage.gameObject.SetActive(false);
+        itemImage.enabled = false;
         itemText.gameObject.SetActive(false);
         text.gameObject.SetActive(false);
         yesButton.gameObject.SetActive(false);
@@ -167,12 +248,28 @@ public class Jugment : MonoBehaviour
     public void NoButton()
     {
         Debug.Log("クリックされた");
+        itemGetFlag = false;
         //画像とテキストを非表示
-        itemImage.gameObject.SetActive(false);
+        itemImage.enabled = false;
         itemText.gameObject.SetActive(false);
         text.gameObject.SetActive(false);
         yesButton.gameObject.SetActive(false);
         noButton.gameObject.SetActive(false);
         Judgment();
+    }
+
+    //次に行くボタンを押した時
+    public void NextButton()
+    {
+        Debug.Log("nextクリックされた");
+        //画像とテキストを非表示
+        playerText.gameObject.SetActive(false);
+        state.gameObject.SetActive(false);
+
+        for (int i = 0; i < playerNum; i++)
+        {
+            player[i].gameObject.SetActive(true);
+        }
+        PlayerNewState();
     }
 }
