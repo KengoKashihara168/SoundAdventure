ae846e522060e339ec5fa0d477da7ad366549934
oonokeiju <fea082400824@gmail.com>
2020-01-28 11:59:03 +0900

    八尺様途中

---
+++ Assets/Onokeiju/Script/Hassyakusama.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hassyakusama : MonoBehaviour
+{
+    public Player player;
+    private StageMap map;
+    private  MapIndex position;
+    MapIndex mapIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(Random.Range(1, 3));
+    }
+
+    private void Move(int num)
+    {
+     
+
+
+     
+    }
+}
